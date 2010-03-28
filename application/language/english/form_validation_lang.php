1308dab608a2274d3f3c17c30947c74d6d304ce6
Saturn <huyanggang@gmail.com>
2010-03-29 04:10:09 +1100

    first commit on github for stblog 0.1.1

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['required'] 			= "%s 不能为空.";
+$lang['isset']				= "%s 必须填写一个值.";
+$lang['valid_email']		= "%s 不是一个有效的Email地址.";
+$lang['valid_emails'] 		= "%s 的所有Email地址均必须有效.";
+$lang['valid_url'] 			= "%s 不是一个有效的URL地址.";
+$lang['valid_ip'] 			= "%s 不是一个有效的IP地址.";
+$lang['min_length']			= "%s 字符的长度不得少于 %s 位.";
+$lang['max_length']			= "%s 字符的长度不得多于 %s 位.";
+$lang['exact_length']		= "%s 字符的长度必须为 %s 位.";
+$lang['alpha']				= "%s 仅能由字母组成.";
+$lang['alpha_numeric']		= "%s 仅能由字母和数字组成.";
+$lang['alpha_dash']			= "%s 仅能由字母/数字/下划线/横线组成.";
+$lang['numeric']			= "%s 仅能由数字组成.";
+$lang['is_numeric']			= "%s 仅能由数字组成.";
+$lang['integer']			= "%s 仅能由整数组成.";
+$lang['matches']			= "%s 与 %s 的值不符.";
+$lang['is_natural']			= "%s 仅能由自然数组成.";
+$lang['is_natural_no_zero']	= "%s 仅能由非零的自然数组成.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./application/language/english/form_validation_lang.php */
